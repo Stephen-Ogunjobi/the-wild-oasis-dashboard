baefeb240d256b06982add27629dab121dbf616d
stephen-ogunjobi <stephenogunjobi400@gmail.com>
2025-07-05 17:35:56 +0800

    computing statistics

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,8 +1,44 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import Spinner from "./../../ui/Spinner";
+import { useRecentStays } from "./useRecentStays";
+import Stats from "./Stats";
+import { useCabins } from "./../cabins/useCabins";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
+  margin-top: 1rem;
 `;
+
+function DashboardLayout() {
+  const { isLoading, bookings } = useRecentBookings();
+  const {
+    stays,
+    confirmedStays,
+    isLoading: isLoadingStay,
+    numDays,
+  } = useRecentStays();
+
+  const { cabins, isLoading: isLoadingCabin } = useCabins();
+
+  if (isLoading || isLoadingStay || isLoadingCabin) return <Spinner />;
+
+  return (
+    <StyledDashboardLayout>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        cabinCount={cabins.length}
+        numDays={numDays}
+      />
+      <div>statistic</div>
+      <div>statistic</div>
+      <div>statistic</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
